708eb441e56548b06a50c292238d285e9456341e
Batarii <cyrillabatari@gmail.com>
2024-12-15 14:57:00 +0700

    profileproject

--- app/src/main/java/com/example/travel_profile/DBHelper.java
+++ app/src/main/java/com/example/travel_profile/DBHelper.java
@@ -2,62 +2,60 @@ package com.example.travel_profile;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
 
     public DBHelper(Context context){
-        super(context,"Userdata.sqlite", null,1);
+        super(context,"Userdata.db", null,2);
 
     }
 
     @Override
     public void onCreate(SQLiteDatabase db)  {
-        db.execSQL("create Table Userdetails(name TEXT primary key, mobile TEXT, email TEXT)");
+        db.execSQL("create Table Userdetails(name TEXT primary key, mobile TEXT, email TEXT, image TEXT)");
+
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("drop Table if exists UserDetails");
+        onCreate(db);
+    }
+    public Boolean checkUserExists(String name) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("Select * from UserDetails where name = ?", new String[]{name});
+        boolean exists = cursor.moveToFirst();
+        cursor.close();
+        db.close();
+        return exists;
     }
 
-    public Boolean updateuserdata(String name, String mobile, String email) {
+    public boolean updateuserdata(String name, String mobile, String email) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", name);
+        contentValues.put("mobile", mobile);
+        contentValues.put("email", email);
+        int result = db.update("UserDetails", contentValues, "name=?", new String[]{name});
+        db.close();
+        return result > 0;
+    }
+
+    public boolean adduserdata(String name, String mobile, String email) {
         SQLiteDatabase DB = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("name", name);
         contentValues.put("mobile", mobile);
         contentValues.put("email", email);
-        DB.update("UserDetails", contentValues, "name=?", new String[]{name});
-        DB.update("UserDetails", contentValues, "mobile=?", new String[]{mobile});
-        DB.update("UserDetails", contentValues, "email=?", new String[]{email});
+        long result = DB.insert("UserDetails", null, contentValues);
         DB.close();
-//        @SuppressLint("Recycle") Cursor cursor = DB.rawQuery("Select * from Userdetails where name = ?", new String[]{name});
-//        if (cursor.getCount() > 0) {
-//            long result = DB.update("UserDetails", contentValues, "name=?", new String[]{name});
-//            return result != -1;
-//        }
-//        else {
-//            return false;
-//        }
-        return null;
+        return result != -1;
     }
-    public Boolean adduserdata(String name, String mobile, String email){
-        SQLiteDatabase DB = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("name", name);
-        contentValues.put("mobile", mobile);
-        contentValues.put("email", email);
-        long result = DB.insert("UserDetails", null,contentValues);
-        if (result==-1){
-            return false;
-        }else {
-            return true;
-        }
-
-    }
-
 }
+
 
 
--- app/src/main/java/com/example/travel_profile/MainActivity.java
+++ app/src/main/java/com/example/travel_profile/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.travel_profile;
 
 import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,11 +15,21 @@ import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.ByteArrayOutputStream;
+
 public class MainActivity extends AppCompatActivity {
+//pembatas
+private SQLiteDatabase mDb;
+private SQLiteOpenHelper mDbHelper;
+//pembatas
 
     TextView name, mobile, email;
     Button update;
     DBHelper DB;
+
+    //
+    de.hdodenhof.circleimageview.CircleImageView profileImageView, cameraButton;
+//
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -23,31 +38,76 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+
         name = findViewById(R.id.name);
         mobile = findViewById(R.id.mobile);
         email = findViewById(R.id.email);
         update = findViewById(R.id.button_update);
 
+
         DB = new DBHelper(this);
+        //perubahan
 
-        update.setOnClickListener(this::onClick);
+//perubahan
 
+
+
+        update.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String nameTXT = name.getText().toString();
+                String mobileTXT = mobile.getText().toString();
+                String emailTXT = email.getText().toString();
+
+                if (DB.checkUserExists(nameTXT)) {
+                    Boolean checkUpdateData = DB.updateuserdata(nameTXT, mobileTXT, emailTXT);
+                    if (checkUpdateData) {
+                        Toast.makeText(MainActivity.this, "Data Updated", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Update Failed", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Boolean checkAddData = DB.adduserdata(nameTXT, mobileTXT, emailTXT);
+                    if (checkAddData) {
+                        Toast.makeText(MainActivity.this, "New Entry Added", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Add Entry Failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
+//
+private static final String IMAGE = "image"; // Nama kolom untuk menyimpan gambar
+    private static final String IMAGE_ID = "image_id"; // Kolom ID gambar
+    private static final String IMAGES_TABLE = "ImagesTable"; // Nama tabel gambar
 
-    private void onClick(View v) {
-        String nameTXT = name.getText().toString();
-        String mobileTXT = mobile.getText().toString();
-        String emailTXT = email.getText().toString();
-
-        Boolean checkupdateData = DB.adduserdata(nameTXT, mobileTXT, emailTXT);
-        if (checkupdateData) {
-            Toast.makeText(MainActivity.this,
-                    "Entry Update",
-                    Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(MainActivity.this,
-                    "New Entry Not Update",
-                    Toast.LENGTH_SHORT).show();
+    public byte[] getImageBytes(Bitmap bitmap) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        return stream.toByteArray();
+    }
+    public void insertImage(byte[] imageBytes){
+        ContentValues cv = new ContentValues();
+        cv.put(IMAGE, imageBytes);
+        mDb.insert(IMAGES_TABLE,null,cv);
+    }
+    public byte[] retreiveImageFromDB(){
+        Cursor cur = mDb.query(false,IMAGES_TABLE,new String[] {IMAGE_ID,IMAGE},
+                null, null, null, null,
+                IMAGE_ID + "DESC","1");
+        if (cur.moveToFirst()){
+            @SuppressLint("Range") byte[] blob = cur.getBlob(cur.getColumnIndex(IMAGE));
+            cur.close();
+            return blob;
         }
+        cur.close();
+        return null;
     }
+
+
+
+
+//
 }
